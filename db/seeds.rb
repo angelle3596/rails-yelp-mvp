10b7e1f53aa45ba24c753fc29355c4f8f241accf
Tori Walker angelle3596@gmail.com <angelle3596@gmail.com>
2019-11-06 16:17:11 -0600

    yelp-mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database"
+Restaurant.destroy_all
+
+restaurants_attributes = [
+  {
+
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '123123456',
+    category:  'indian'
+  },
+
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '3458790967',
+    category:  'chinese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '9876540987',
+    category:  'italian'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '3478730987',
+    category:  'japanese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '7650981765',
+    category:  'french'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
